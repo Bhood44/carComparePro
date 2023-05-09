41091f88c4a5a298fee0a6a03539f78e1fe45762
Ben <hdbjmn@gmail.com>
2023-05-09 17:13:17 -0500

    Set up upstream

---
+++ src/main/java/com/example/car_compare_pro/CarCompareProApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_compare_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCompareProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCompareProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/car_compare_pro/CarCompareProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_compare_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCompareProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
